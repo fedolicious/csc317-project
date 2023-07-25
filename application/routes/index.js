f1d0d37e1a56be071e17e6dd657f190187faf9de
fedolicious <athompson5@sfsu.edu>
2023-07-24 22:20:37 -0700

    milestone 2 complete

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Alfred Thompson" });
+  res.render('index', { title: 'Home', name:"Alfred Thompson", js:["index.js"] });
+});
+router.get("/login", function(req, res, next) {
+  res.render("login", {title: 'Log In'});
+});
+router.get("/postvideo", function(req, res, next) {
+  res.render("postvideo", {title: "Post Video"});
+});
+router.get("/registration", function(req, res, next) {
+  res.render("registration", {title: "Register", js:["registration.js"]});
+});
+router.get("/viewpost/:id(\\d+)", function(req, res, next) {
+  res.render("viewpost", {title: "View Post"});
 });
 
 module.exports = router;
