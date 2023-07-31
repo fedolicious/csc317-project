ce531d30bea56d95a1628374c46603c9932dde3f
fedolicious <jalfedral@gmail.com>
2023-07-31 05:33:19 -0700

    milestone 3 complete

--- application/config/database.js
+++ application/config/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    username: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     queueLimit: 0,
--- application/routes/users.js
+++ application/routes/users.js
@@ -4,27 +4,55 @@ var router = express.Router();
 const database = require("../config/database");
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//     res.send('respond with a resource');
-// });
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
 router.post("/registration", async function(req, res, next) {
     let {username,email,password} = req.body;
     try {
-        let results;
-        [results, _] = await database.execute(`select id from users where username=?`,[username]);
-        if(results && results.length > 0) {
+        let result;
+        [result, _] = await database.execute(`select id from users where username=?`, [username]);
+        if(result && result.length > 0) {
             console.log(`username ${username} is taken`);
             return res.redirect("/registration");
         }
-        [results, _] = await database.execute(`select id from users where email=?`,[email]);
-        if(results && results.length > 0) {
+        [result, _] = await database.execute(`select id from users where email=?`, [email]);
+        if(result && result.length > 0) {
             console.log(`email ${email} is taken`);
             return res.redirect("/registration");
         }
-        return res.send(`username ${username} and email ${email} are available`);
+        [result, _] = await database.execute(`insert into users (username, email, password) value (?,?,?)`, [username,email,password]);
+        if(result && result.affectedRows === 1) {
+            return res.redirect("/login")
+        } else {
+            return res.redirect("/registration");
+        }
+        // return res.send(`username ${username} and email ${email} are available`);
     } catch(err) {
         next(err);
+        return res.send("q");
     }
+});
+router.post("/login", async function(req, res, next) {
+    let {username,password} = req.body;
+    let result
+    [result, _] = await database.execute(`select password from users where username=?`, [username]);
+    if(result && result.length !== 1) {
+        console.log(`username ${username} does not exist`);
+        return res.redirect("/login");
+    }
+    if(result[0].password !== password) {
+        console.log(`incorrect password`);
+        return res.redirect("/login");
+    }
+    console.log("logged in successfully");
+    // res.render('index', {title: 'Home', name: "Alfred Thompson", js: ["index.js"]});
+    return res.redirect("/");
+});
+router.get("/logout", async function(req, res, next) {
+    console.log("logged out successfully");
+    // res.render('index', {title: 'Home', name: "Alfred Thompson", js: ["index.js"]});
+    return res.redirect("/");
 });
 
 module.exports = router;
